3608265edc0c3e31b137970d4936d0086be99ad2
vladhoncharenko <vladhoncharenko@gmail.com>
2017-05-21 01:18:28 +0300

    Config was added in Helpers folder.

--- app/js/controllers/map-controller.js
+++ app/js/controllers/map-controller.js
@@ -1,30 +1,9 @@
 'use strict';
 (function (angular) {
-    angular.module('sumyBugTracker').controller('MapController', ['$scope', '$http', 'dataContext', function ($scope, $http, dataContext) {
+    angular.module('sumyBugTracker').controller('MapController', ['$scope', '$http', 'dataContext','config', function ($scope, $http, dataContext, config) {
 
         let map, geocoder;
         let infoWindow;
-
-        // Get map style here: https://mapkit.io/editor
-        let mapStyle = [{
-            "featureType": "landscape",
-            "stylers": [{"hue": "#FFBB00"}, {"saturation": 43.400000000000006}, {"lightness": 37.599999999999994}, {"gamma": 1}]
-        }, {
-            "featureType": "road.highway",
-            "stylers": [{"hue": "#FFC200"}, {"saturation": -61.8}, {"lightness": 45.599999999999994}, {"gamma": 1}]
-        }, {
-            "featureType": "road.arterial",
-            "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 51.19999999999999}, {"gamma": 1}]
-        }, {
-            "featureType": "road.local",
-            "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 52}, {"gamma": 1}]
-        }, {
-            "featureType": "water",
-            "stylers": [{"hue": "#0078FF"}, {"saturation": -13.200000000000003}, {"lightness": 2.4000000000000057}, {"gamma": 1}]
-        }, {
-            "featureType": "poi",
-            "stylers": [{"hue": "#00FF6A"}, {"saturation": -1.0989010989011234}, {"lightness": 11.200000000000017}, {"gamma": 1}]
-        }];
 
         $scope.initialize = function () {
             let mapOptions = {
@@ -33,7 +12,7 @@
                 mapTypeId: 'roadmap',
                 streetViewControl: false,
                 zoomControl: false,
-                styles: mapStyle,
+                styles: config.mapStyle,
             };
             geocoder = new google.maps.Geocoder();
             map = new google.maps.Map(document.getElementById('map'), mapOptions);
---
+++ app/js/helpers/config.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Vlad on 21.05.2017.
+ */
+'use strict';
+(function (angular) {
+
+    angular.module('sumyBugTracker').factory('config', [function () {
+        let factory = {};
+
+        // Get map style here: https://mapkit.io/editor
+        factory.mapStyle = [{
+            "featureType": "landscape",
+            "stylers": [{"hue": "#FFBB00"}, {"saturation": 43.400000000000006}, {"lightness": 37.599999999999994}, {"gamma": 1}]
+        }, {
+            "featureType": "road.highway",
+            "stylers": [{"hue": "#FFC200"}, {"saturation": -61.8}, {"lightness": 45.599999999999994}, {"gamma": 1}]
+        }, {
+            "featureType": "road.arterial",
+            "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 51.19999999999999}, {"gamma": 1}]
+        }, {
+            "featureType": "road.local",
+            "stylers": [{"hue": "#FF0300"}, {"saturation": -100}, {"lightness": 52}, {"gamma": 1}]
+        }, {
+            "featureType": "water",
+            "stylers": [{"hue": "#0078FF"}, {"saturation": -13.200000000000003}, {"lightness": 2.4000000000000057}, {"gamma": 1}]
+        }, {
+            "featureType": "poi",
+            "stylers": [{"hue": "#00FF6A"}, {"saturation": -1.0989010989011234}, {"lightness": 11.200000000000017}, {"gamma": 1}]
+        }];
+
+        return factory;
+    }]);
+}(angular));
