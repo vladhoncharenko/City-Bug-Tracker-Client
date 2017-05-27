53c34e9a9bdcce17cf818df6fd668712b951f5f2
vladhoncharenko <vladhoncharenko@gmail.com>
2017-05-27 18:22:14 +0300

    All bugs page was added.

--- app/js/controllers/all-bugs-controller.js
+++ app/js/controllers/all-bugs-controller.js
@@ -5,14 +5,33 @@
 'use strict';
 (function (angular) {
     angular.module('sumyBugTracker').controller('AllBugsController', ['$scope', '$http', 'dataContext', 'config', function ($scope, $http, dataContext, config) {
-
+        let map, geocoder;
+        let infoWindow;
         $scope.initialize = function () {
+
+            let mapOptions = {
+                center: new google.maps.LatLng(50.908424, 34.811541),
+                zoom: 14,
+                disableDefaultUI: true,
+                mapTypeId: 'roadmap',
+                streetViewControl: false,
+                zoomControl: false,
+                styles: config.mapStyle,
+            };
+            geocoder = new google.maps.Geocoder();
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            infoWindow = new google.maps.InfoWindow();
+            google.maps.event.addListener(map, 'click', function () {
+                infoWindow.close();
+            });
+
+
             dataContext.getAllBugsData().then(bugsData => {
                 bugsData.forEach(item=>{
                     item.date = new Date(item.date * 1000).toISOString().slice(0, 10);
                 });
                 $scope.bugs = bugsData;
-                console.log($scope.bugs)
+                $scope.$broadcast('rebuild:me');
             });
         };
 
