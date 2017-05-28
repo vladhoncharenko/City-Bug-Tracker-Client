92e670351b1ae0310b5de8ce8204ce19a358f8c1
vladhoncharenko <vladhoncharenko@gmail.com>
2017-05-28 09:57:09 +0300

    Geocoder was added.

--- app/js/controllers/bug-controller.js
+++ app/js/controllers/bug-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 (function (angular) {
-    angular.module('sumyBugTracker').controller('BugController', ['$scope', '$http', 'dataContext', 'config', function ($scope, $http, dataContext, config) {
+    angular.module('sumyBugTracker').controller('BugController', ['$scope', '$http', 'dataContext', 'config','$timeout', function ($scope, $http, dataContext, config,$timeout) {
         let map, geocoder;
         let infoWindow;
         $scope.initialize = function (bugId) {
@@ -23,6 +23,19 @@
             dataContext.getBugData(bugId).then(bugData => {
                 bugData.date = new Date(bugData.date * 1000).toISOString().slice(0, 10);
                 $scope.bug = bugData;
+
+                let latlng = new google.maps.LatLng(bugData.coordinates[0], bugData.coordinates[1]);
+                geocoder.geocode({'latLng': latlng}, function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        $timeout(function () {
+                            $scope.adress = results[0].formatted_address;
+                        });
+                    }else{
+                        $timeout(function () {
+                            $scope.adress=bugData.coordinates;
+                        });
+                    }
+                });
             });
         };
 
