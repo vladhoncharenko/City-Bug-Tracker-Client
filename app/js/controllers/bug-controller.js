26766908c1c8469cf1878c0837494e42582dcf99
vladhoncharenko <vladhoncharenko@gmail.com>
2017-05-28 08:54:10 +0300

    Map was added to bug page.

--- app/js/controllers/bug-controller.js
+++ app/js/controllers/bug-controller.js
@@ -1,8 +1,25 @@
 'use strict';
 (function (angular) {
     angular.module('sumyBugTracker').controller('BugController', ['$scope', '$http', 'dataContext', 'config', function ($scope, $http, dataContext, config) {
-
+        let map, geocoder;
+        let infoWindow;
         $scope.initialize = function (bugId) {
+            let mapOptions = {
+                center: new google.maps.LatLng(50.908424, 34.811541),
+                zoom: 14,
+                disableDefaultUI: true,
+                mapTypeId: 'roadmap',
+                streetViewControl: false,
+                zoomControl: false,
+                styles: config.mapStyle,
+            };
+            geocoder = new google.maps.Geocoder();
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            infoWindow = new google.maps.InfoWindow();
+            google.maps.event.addListener(map, 'click', function () {
+                infoWindow.close();
+            });
+
             dataContext.getBugData(bugId).then(bugData => {
                 bugData.date = new Date(bugData.date * 1000).toISOString().slice(0, 10);
                 $scope.bug = bugData;
